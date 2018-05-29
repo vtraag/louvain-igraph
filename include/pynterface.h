47ff4f4ebcd51a7838f06e8f11f436febcd6edec
ragibson <ryanalexandergibson@gmail.com>
2018-05-29 10:09:40 -0400

    Resolve some type issues and expose constructor to Python

--- include/pynterface.h
+++ include/pynterface.h
@@ -34,6 +34,7 @@ extern "C"
       {"_new_CPMVertexPartition",                                   (PyCFunction)_new_CPMVertexPartition,                                   METH_VARARGS | METH_KEYWORDS, ""},
       {"_new_RBERVertexPartition",                                  (PyCFunction)_new_RBERVertexPartition,                                  METH_VARARGS | METH_KEYWORDS, ""},
       {"_new_RBConfigurationVertexPartition",                       (PyCFunction)_new_RBConfigurationVertexPartition,                       METH_VARARGS | METH_KEYWORDS, ""},
+      {"_new_RBConfigurationVertexPartitionWeightedLayers",         (PyCFunction)_new_RBConfigurationVertexPartitionWeightedLayers,         METH_VARARGS | METH_KEYWORDS, ""},
 
       {"_MutableVertexPartition_diff_move",                         (PyCFunction)_MutableVertexPartition_diff_move,                         METH_VARARGS | METH_KEYWORDS, ""},
       {"_MutableVertexPartition_move_node",                         (PyCFunction)_MutableVertexPartition_move_node,                         METH_VARARGS | METH_KEYWORDS, ""},
--- src/python_partition_interface.cpp
+++ src/python_partition_interface.cpp
@@ -611,12 +611,12 @@ extern "C"
 
     double resolution_parameter = 1.0;
 
-    static char* kwlist[] = {"graph","layer_vec","degrees_by_layers","initial_membership", "weights", "resolution_parameter", NULL};
+    static char* kwlist[] = {"graph", "layer_vec", "degrees_by_layers", "initial_membership", "weights", "resolution_parameter", NULL};
 
-    if (!PyArg_ParseTupleAndKeywords(args, keywds, "O|OOd", kwlist,
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "O|OOOOd", kwlist,
                                      &py_obj_graph, &py_layer_vec, &py_degrees_by_layers,
-                                      &py_initial_membership, &py_weights, &resolution_parameter))
-        return NULL;
+                                     &py_initial_membership, &py_weights, &resolution_parameter))
+      return NULL;
 
     try
     {
@@ -635,9 +635,9 @@ extern "C"
       {
         PyObject* py_item = PyList_GetItem(py_layer_vec, v);
         #ifdef IS_PY3K
-        if (PyFloat_Check(py_item))
+        if (PyLong_Check(py_item))
         #else
-        if (PyFloat_Check(py_item) || PyLong_Check(py_item))
+        if (PyInt_Check(py_item) || PyLong_Check(py_item))
         #endif
         {
           layer_vec[v] = PyLong_AsLong(py_item);
@@ -649,7 +649,7 @@ extern "C"
         }
       }
 
-      vector< vector<double> > degrees_by_layers;
+      vector<vector<double> > degrees_by_layers;
       #ifdef DEBUG
         cerr << "Reading layer total weights." << endl;
       #endif
@@ -658,28 +658,27 @@ extern "C"
       degrees_by_layers.resize(n);
       for (size_t v = 0; v < n; v++)
       {
-            PyObject* py_item = PyList_GetItem(py_degrees_by_layers, v);
-            m=PyList_Size(py_item);
-            degrees_by_layers[v].resize(m);
-            for(size_t w=0;w<m;w++)
-            {
-            PyObject* py_item_item = PyList_GetItem(py_item, w);
-           #ifdef IS_PY3K
+        PyObject* py_item = PyList_GetItem(py_degrees_by_layers, v);
+        m = PyList_Size(py_item);
+        degrees_by_layers[v].resize(m);
+        for (size_t w = 0; w < m; w++)
+        {
+          PyObject* py_item_item = PyList_GetItem(py_item, w);
+          #ifdef IS_PY3K
           if (PyFloat_Check(py_item_item) || PyLong_Check(py_item_item))
           #else
           if (PyFloat_Check(py_item_item) || PyInt_Check(py_item_item) || PyLong_Check(py_item_item))
           #endif
-            {
-              degrees_by_layers[v][w] = PyFloat_AsDouble(py_item_item);
-            }
-            else
-            {
-              PyErr_SetString(PyExc_TypeError, "Expected integer value for membership vector.");
-              return NULL;
-        }
+          {
+            degrees_by_layers[v][w] = PyFloat_AsDouble(py_item_item);
+          }
+          else
+          {
+            PyErr_SetString(PyExc_TypeError, "Expected float value for degree vector.");
+            return NULL;
+          }
         }
       }
-
 
       // If necessary create an initial partition
       if (py_initial_membership != NULL && py_initial_membership != Py_None)
@@ -713,7 +712,7 @@ extern "C"
         partition = new RBConfigurationVertexPartitionWeightedLayers(graph, layer_vec, degrees_by_layers, initial_membership, resolution_parameter);
       }
       else
-        partition = new RBConfigurationVertexPartitionWeightedLayers(graph,layer_vec,degrees_by_layers, resolution_parameter);
+        partition = new RBConfigurationVertexPartitionWeightedLayers(graph, layer_vec, degrees_by_layers, resolution_parameter);
 
       // Do *NOT* forget to remove the graph upon deletion
       partition->destructor_delete_graph = true;
@@ -725,7 +724,7 @@ extern "C"
 
       return py_partition;
     }
-    catch (std::exception const & e )
+    catch (std::exception const &e)
     {
       string s = "Could not construct partition: " + string(e.what());
       PyErr_SetString(PyExc_BaseException, s.c_str());
